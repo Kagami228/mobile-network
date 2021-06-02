38d92911ca42f961a2c6a31eb1688c3728239247
Kagami228 <sasha.dovgan@gmail.com>
2021-06-02 21:40:03 +0300

    command

--- main.cpp
+++ main.cpp
@@ -1,210 +1,135 @@
-/**
- * @file cpp_application_changes_example.cpp
- * @author Mislav Novakovic <mislav.novakovic@sartura.hr>
- * @author Michal Vasko <mvasko@cesnet.cz>
- * @brief Example application that uses sysrepo as the configuration datastore. It
- * prints the changes made in running data store.
- *
- * @copyright
- * Copyright 2016 - 2019 Deutsche Telekom AG.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-
-
-#include <unistd.h>
-#include <iostream>
-#include <signal.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "libsysrepocpp/headers/Session.hpp"
-
-#define MAX_LEN 100
-
-using namespace std;
-
-volatile int exit_application = 0;
-
-/* Helper function for printing changes given operation, old and new value. 
-static void
-print_change(sysrepo::S_Change change) {
-    cout << endl;
-    switch(change->oper()) {
-    case SR_OP_CREATED:
-        if (nullptr != change->new_val()) {
-           cout <<"CREATED: ";
-           cout << change->new_val()->to_string();
-        }
-        break;
-    case SR_OP_DELETED:
-        if (nullptr != change->old_val()) {
-           cout << "DELETED: ";
-           cout << change->old_val()->to_string();
-        }
-    break;
-    case SR_OP_MODIFIED:
-        if (nullptr != change->old_val() && nullptr != change->new_val()) {
-           cout << "MODIFIED: ";
-           cout << "old value ";
-           cout << change->old_val()->to_string();
-           cout << "new value ";
-           cout << change->new_val()->to_string();
-        }
-    break;
-    case SR_OP_MOVED:
-        if (nullptr != change->old_val() && nullptr != change->new_val()) {
-           cout << "MOVED: ";
-           cout << change->new_val()->xpath();
-           cout << " after ";
-           cout << change->old_val()->xpath();
-        } else if (nullptr != change->new_val()) {
-           cout << "MOVED: ";
-           cout << change->new_val()->xpath();
-           cout << " first";
-        }
-    break;
-    }
-}
-
-/* Function to print current configuration state.
- * It does so by loading all the items of a session and printing them out. 
-static void
-print_current_config(sysrepo::S_Session session, const char *module_name)
-{
-    char select_xpath[MAX_LEN];
-    try {
-        snprintf(select_xpath, MAX_LEN, "/%s:*//*", module_name);
-
-        auto values = session->get_items(&select_xpath[0]);
-        if (values == nullptr)
-            return;
-
-        for(unsigned int i = 0; i < values->val_cnt(); i++)
-            cout << values->val(i)->to_string();
-    } catch( const std::exception& e ) {
-        cout << e.what() << endl;
-    }
-}
-
-/* Helper function for printing events. 
-const char *ev_to_str(sr_event_t ev) {
-    switch (ev) {
-    case SR_EV_CHANGE:
-        return "change";
-    case SR_EV_DONE:
-        return "done";
-    case SR_EV_ABORT:
-    default:
-        return "abort";
-    }
-}
-
-static void
-sigint_handler(int signum)
-{
-    exit_application = 1;
-}
-
-/* Notable difference between c implementation is using exception mechanism for open handling unexpected events.
- * Here it is useful because `Conenction`, `Session` and `Subscribe` could throw an exception. 
-int
-main(int argc, char **argv)
-{
-    const char *module_name = "ietf-interfaces";
-    try {
-        if (argc > 1) {
-            module_name = argv[1];
-        } else {
-            cout << "\nYou can pass the module name to be subscribed as the first argument" << endl;
-        }
-
-        cout << "hello"<< endl;
-        cout << "Application will watch for changes in " << module_name << endl;
-        /* connect to sysrepo 
-        cout << "connecting to sysrepo" <<endl;
-        auto conn = std::make_shared<sysrepo::Connection>();
-        
-
-        /* start session 
-        cout << "starting session" << endl;
-        auto sess = std::make_shared<sysrepo::Session>(conn);
-
-        /* subscribe for changes in running config 
-        auto subscribe = std::make_shared<sysrepo::Subscribe>(sess);
-        auto cb = [] (sysrepo::S_Session sess, const char *module_name, const char *xpath, sr_event_t event,
-            uint32_t request_id) {
-            char change_path[MAX_LEN];
-
-            try {
-                cout << "\n\n ========== Notification " << ev_to_str(event) << " =============================================";
-                if (SR_EV_CHANGE == event) {
-                    cout << "\n\n ========== CONFIG HAS CHANGED, CURRENT RUNNING CONFIG: ==========\n" << endl;
-                    print_current_config(sess, module_name);
-                }
-
-                cout << "\n\n ========== CHANGES: =============================================\n" << endl;
-
-                snprintf(change_path, MAX_LEN, "/%s://.", module_name);
-
-                auto it = sess->get_changes_iter(change_path);
-
-                while (auto change = sess->get_change_next(it)) {
-                    print_change(change);
-                }
-
-                cout << "\n\n ========== END OF CHANGES =======================================\n" << endl;
-
-            } catch( const std::exception& e ) {
-                cout << e.what() << endl;
-            }
-            return SR_ERR_OK;
-
-        };
-        subscribe->module_change_subscribe(module_name, cb);
-
-        /* read running config 
-        cout << "\n\n ========== READING RUNNING CONFIG: ==========\n" << endl;
-        print_current_config(sess, module_name);
-
-        /* loop until ctrl-c is pressed / SIGINT is received 
-        signal(SIGINT, sigint_handler);
-        while (!exit_application) {
-            sleep(1000);  /* or do some more useful work... 
-        }
-
-        cout << "Application exit requested, exiting." << endl;
-
-    } catch( const std::exception& e ) {
-        cout << e.what() << endl;
-        return -1;
-    }
-
-    return 0;
-}
-*/
 #include <iostream>
 
+void Registr_user(std::string &phone, std::string &user)
+{
+    std::cout<<user<<" was register by " <<phone<<std::endl;
+}
+void Unregistr(){
+    std::cout<<"You unregistrd your phone"<<std::endl;
+}
+void Calling_to_phone(std::string phone)
+{
+    std::cout<<"calling..."<<std::endl;
+}
+void Answer()
+{
+    std::cout<<"conversation"<<std::endl;
+}
+void CallEnd(){
+    std::cout<<"callEnd"<<std::endl;
+}
+void Reject(){
+    std::cout<<"rejected"<<std::endl;
+}
+void ChangeName(std::string &name){
+    std::cout<<"Your new name "<<name<<std::endl;
+}
 
-int main(){
+int main()
+{
     std::string command;
-    while(std::getline(std::cin,command)){
+    std::getline(std::cin,command);
+    while(true)
+    {
         std::string cm="";
-        for(int i=0; i<command.size();i++){
+        for(int i=0; i<command.size();i++)
+        {
             
-            if(command[i]==' '){
+            if(command[i]==' ' or (i=command.size()-1))
+            {
+                if(i=command.size()-1)
+                {
+                    cm=cm+command[i];
+                }
                 if (cm=="register")
                 {
+                    std::string phone="";
+                    for(int j=i; j<command.size();j++)
+                    {
+                        if(command[j]==' ' or (j=command.size()-1))
+                        {
+                            if(j=command.size()-1)
+                            {
+                                phone=phone+command[j];
+                            }
+                            std::string name="";
+                            for(int o=j; o<command.size();o++)
+                            {
+                                if(command[o]==' ' or (o=command.size()-1))
+                                {
+                                    if(o=command.size()-1)
+                                    {
+                                        name=name+command[o];
+                                    }   
+                                    Registr_user(phone,name);
+                                }else{
+                                    name=name+command[o];
+                                }
+                            }
+                        }else{
+                            phone=phone+command[j];
+                        }
+                    }
                     
+                } else if(cm=="unregistr")
+                {
+                    Unregistr();
+                       
+                }
+                else if(cm=="call")
+                {
+                    std::string calling="";
+                    for(int j=i; j<command.size();j++)
+                    {
+                        if(command[j]==' ' or (j=command.size()-1))
+                        {
+                            if(j=command.size()-1)
+                            {
+                                calling=calling+command[j];
+                            }
+                            Calling_to_phone(calling);
+                        }else
+                        {
+                            calling=calling=command[j];
+                        }
+                    }
+                }
+                else if(cm=="answer")
+                {
+                    Answer();
+                       
+                }
+                else if(cm=="callEnd")
+                {
+                    CallEnd();
+                       
+                }
+                else if(cm=="reject")
+                {
+                    Reject();
+                       
+                }
+                else if(cm=="name")
+                {
+                    std::string newName="";
+                    for(int j=i; j<command.size();j++)
+                    {
+                        if(command[j]==' ' or (j=command.size()-1))
+                        {
+                            if(j=command.size()-1)
+                            {
+                                newName=newName+command[j];
+                            }
+                            ChangeName(newName);
+                        }else
+                        {
+                            newName=newName=command[j];
+                        }
+                    }
+                       
+                }
+                else if(cm=="exit"){
+                    break;
                 }
             }
             else {
@@ -212,4 +137,5 @@ int main(){
             }
         }
     }
+    return 0;
 }
